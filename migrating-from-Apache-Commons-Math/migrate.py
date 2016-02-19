f4c3f58b95f22e8647236152be6a63a7ebbeb742
Thomas Neidhart <thomas.neidhart@gmail.com>
2016-02-19 19:09:57 +0100

    Add missing support for class substitutions, add more logging.

--- migrating-from-Apache-Commons-Math/migrate.py
+++ migrating-from-Apache-Commons-Math/migrate.py
@@ -21,6 +21,13 @@ def fnmatch(fname, extensions):
 			return True
 	return False
 
+def log(msg, args, newline = True):
+	if args.verbose == True:
+		if newline == True:
+			print msg
+		else:
+			print msg,
+
 def processDir(rootDir, args):
 	for dirName, subdirList, fileList in os.walk(rootDir):
 		for fname in fileList:
@@ -36,28 +43,34 @@ def processDir(rootDir, args):
 
 
 def processFile(file, args):
+	log("processing file " + file, args, False)
+
 	g = os.path.basename(file)
-	tempfile = os.path.join(args.tempdir, g)
+	tmpfile = os.path.join(args.tempdir, g)
 	with open(file, "r") as sources:
 		lines = sources.readlines()
-	with open(tempfile, "w") as sources:
+	with open(tmpfile, "w") as sources:
 		for line in lines:
-			sources.write(packageReplace(line, args))
+			line = packageReplace(line, args)
+			line = classesReplace(line, args)
+			sources.write(line)
 
-	if filecmp.cmp(file, tempfile) == False:
+	if filecmp.cmp(file, tmpfile) == False:
+		log("-> changed", args)
+
 		if args.dry_run == True:
-			print file
-			os.remove(tempfile)
+			if args.verbose == False:
+				print file
+			os.remove(tmpfile)
 		else:
 			if args.save == True:
-				print "saving file" + file
 				shutil.move(file, file + ".orig")
-
-			shutil.move(tempfile, file)
+			shutil.move(tmpfile, file)
+	else:
+		log("-> unchanged", args)
 
 def packageReplace(line, args):
-	for pattern in args.packages_subst_pattern:
-		p = pattern.split(':')
+	for p in args.packages_subst_pattern:
 		line = re.sub(p[1], p[2], line)
 	return line
 
@@ -68,11 +81,28 @@ def parsePackageSubstitutionRules(args):
 	for line in lines:
 		pattern = re.sub(r'^\s*"([^"]*)"\s*"([^"]*)"\s*', r's:\1:\2', line)
 		if pattern.startswith('s:'):
-			args.packages_subst_pattern.append(pattern)
+			args.packages_subst_pattern.append(pattern.split(':'))
+
+def classesReplace(line, args):
+	for p in args.classes_subst_pattern:
+		line = re.sub(p[1], p[2], line)
+	return line
+
+def parseClassSubstitutionRules(args):
+	with open(args.classes_subst, "r") as sources:
+		lines = sources.readlines()
+
+	# substitution rules for class names, taking care of *not* substituting
+	# names where the pattern is only a substring of the name in the file
+	# so if we want to substitute OriginalClass with ReplacementClass, we
+	# do not want to substitute MyOwnOriginalClass or OriginalClassExtended
+	for line in lines:
+		pattern = re.sub(r'^\s*([A-Za-z_][A-Za-z0-9_]*)\s*([A-Za-z_][A-Za-z0-9_]*)\s*$', r's:(^|[^A-Za-z0-9_])\1([^A-Za-z0-9_]|$):\\1\2\\2', line)
+		if pattern.startswith('s:'):
+			args.classes_subst_pattern.append(pattern.split(':'))
 
 
 # start main
-# TODO: handle class substitutions
 
 parser = argparse.ArgumentParser(prog='migrate')
 parser.add_argument('--dir', action='append', required=True, type=lambda x: is_valid_directory(parser, x))
@@ -82,6 +112,7 @@ parser.add_argument('--nosave', action='store_false', dest='save', default=True)
 parser.add_argument('--packages-subst', default=None)
 parser.add_argument('--classes-subst', default=None, required=True)
 parser.add_argument('--dry-run', action='store_true', default=False)
+parser.add_argument('--verbose', '-v', action='store_true', dest='verbose', default=False)
 
 args = parser.parse_args()
 
@@ -95,9 +126,13 @@ if args.packages_subst == None:
 else:
 	parsePackageSubstitutionRules(args)
 
+args.classes_subst_pattern = []
+parseClassSubstitutionRules(args)
+
 #print args
 
 # process the files
 for dir in args.dir:
-	print 'Processing files in directory ' + dir
+	log("Processing files in dir " + dir, args)
 	processDir(dir, args)
+
