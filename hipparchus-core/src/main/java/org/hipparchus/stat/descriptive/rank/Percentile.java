2665c2568fbda7ac93b60ae1f01366344d003057
Thomas Neidhart <thomas.neidhart@gmail.com>
2016-04-23 09:45:23 +0200

    Further simplify the PivotingStrategy: remove Random strategy as it is pretty much meaningless and unused, change to an enum which brings singleton and serialization for free.

--- hipparchus-core/src/main/java/org/hipparchus/stat/descriptive/rank/Percentile.java
+++ hipparchus-core/src/main/java/org/hipparchus/stat/descriptive/rank/Percentile.java
@@ -148,7 +148,7 @@ public class Percentile extends AbstractUnivariateStatistic
      */
     public Percentile(final double quantile) throws MathIllegalArgumentException {
         this(quantile, EstimationType.LEGACY, NaNStrategy.REMOVED,
-             new KthSelector(PivotingStrategy.medianOf3()));
+             new KthSelector(PivotingStrategy.MEDIAN_OF_3));
     }
 
     /**
