de3938cb0a655483f58d395501b222efdf568dd7
Luc Maisonobe <luc@orekit.org>
2018-03-16 22:24:47 +0100

    Fixed import.

--- hipparchus-filtering/src/test/java/org/hipparchus/filtering/kalman/linear/LinearKalmanFilterTest.java
+++ hipparchus-filtering/src/test/java/org/hipparchus/filtering/kalman/linear/LinearKalmanFilterTest.java
@@ -23,7 +23,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
