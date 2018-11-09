9b171e64bba168c0a252fdd53c805fff67077764
Luc Maisonobe <luc@orekit.org>
2018-11-09 19:54:45 +0100

    Fixed PMD violations for the hipparchus-clustering module.

--- hipparchus-clustering/src/main/java/org/hipparchus/clustering/DoublePoint.java
+++ hipparchus-clustering/src/main/java/org/hipparchus/clustering/DoublePoint.java
@@ -43,7 +43,7 @@ public class DoublePoint implements Clusterable, Serializable {
      *
      * @param point the n-dimensional point in double space
      */
-    public DoublePoint(final double[] point) {
+    public DoublePoint(final double[] point) { // NOPMD - storage of array reference is intentional and documented here
         this.point = point;
     }
 
@@ -64,7 +64,7 @@ public class DoublePoint implements Clusterable, Serializable {
     /** {@inheritDoc} */
     @Override
     public double[] getPoint() {
-        return point;
+        return point.clone();
     }
 
     /** {@inheritDoc} */
--- hipparchus-clustering/src/main/java/org/hipparchus/clustering/LocalizedClusteringFormats.java
+++ hipparchus-clustering/src/main/java/org/hipparchus/clustering/LocalizedClusteringFormats.java
@@ -81,7 +81,7 @@ public enum LocalizedClusteringFormats implements Localizable {
                 final String translated = bundle.getString(name());
                 if ((translated != null) &&
                     (translated.length() > 0) &&
-                    (!translated.toLowerCase().contains("missing translation"))) {
+                    (!translated.toLowerCase(locale).contains("missing translation"))) {
                     // the value of the resource is the translated format
                     return translated;
                 }
