5a2562ff1c7d4160b7bcaf70c801a8c13020ebba
Luc Maisonobe <luc@orekit.org>
2020-05-19 08:39:32 +0200

    Fixed SonarQube warning.

--- hipparchus-core/src/main/java/org/hipparchus/linear/RealMatrix.java
+++ hipparchus-core/src/main/java/org/hipparchus/linear/RealMatrix.java
@@ -962,7 +962,7 @@ public interface RealMatrix extends AnyMatrix {
      * @return a new matrix.
      * @since 1.7
      */
-    default public RealMatrix map(UnivariateFunction function) {
+    public default RealMatrix map(UnivariateFunction function) {
         return copy().mapToSelf(function);
     }
 
@@ -973,7 +973,7 @@ public interface RealMatrix extends AnyMatrix {
      * @return a reference to this matrix.
      * @since 1.7
      */
-    default public RealMatrix mapToSelf(final UnivariateFunction function) {
+    public default RealMatrix mapToSelf(final UnivariateFunction function) {
         walkInOptimizedOrder(new RealMatrixChangingVisitor() {
 
             /** {@inheritDoc} */
