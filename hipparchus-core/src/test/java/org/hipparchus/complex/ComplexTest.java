3e89246f2ef20436ea9c0325e9e0a625e5c1893a
Luc Maisonobe <luc@orekit.org>
2020-11-16 20:45:06 +0100

    Fixed LU-decomposition issue with signed zero complex numbers.
    Fixes #104

--- hipparchus-core/src/test/java/org/hipparchus/complex/ComplexTest.java
+++ hipparchus-core/src/test/java/org/hipparchus/complex/ComplexTest.java
@@ -1979,6 +1979,21 @@ public class ComplexTest extends CalculusFieldElementAbstractTest<Complex> {
     }
 
     @Test
+    public void testSignedZeroEquality() {
+
+        Assert.assertTrue(new Complex(-0.0, -0.0).isZero());
+        Assert.assertTrue(new Complex(-0.0, +0.0).isZero());
+        Assert.assertTrue(new Complex(+0.0, -0.0).isZero());
+        Assert.assertTrue(new Complex(+0.0, +0.0).isZero());
+
+        Assert.assertFalse(new Complex(-0.0, -0.0).equals(Complex.ZERO));
+        Assert.assertFalse(new Complex(-0.0, +0.0).equals(Complex.ZERO));
+        Assert.assertFalse(new Complex(+0.0, -0.0).equals(Complex.ZERO));
+        Assert.assertTrue(new Complex(+0.0, +0.0).equals(Complex.ZERO));
+
+    }
+
+    @Test
     public void testSerial() {
         Complex z = new Complex(3.0, 4.0);
         Assert.assertEquals(z, UnitTestUtils.serializeAndRecover(z));
