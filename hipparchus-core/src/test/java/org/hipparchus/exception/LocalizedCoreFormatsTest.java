a9b601fac94f80812a7f6e389bd36db5584684dc
Bryan Cazabonne <bryan.cazabonne@c-s.fr>
2022-07-26 12:04:58 +0200

    Fixed failing test.

--- hipparchus-core/src/test/java/org/hipparchus/exception/LocalizedCoreFormatsTest.java
+++ hipparchus-core/src/test/java/org/hipparchus/exception/LocalizedCoreFormatsTest.java
@@ -30,7 +30,7 @@ public class LocalizedCoreFormatsTest extends LocalizedFormatsAbstractTest {
 
     @Override
     protected int getExpectedNumber() {
-        return 181;
+        return 183;
     }
 
 }
