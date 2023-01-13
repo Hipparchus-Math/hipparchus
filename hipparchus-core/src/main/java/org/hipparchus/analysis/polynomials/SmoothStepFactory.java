61501b15ed2f55e7d5ac16315e75f1493f81c373
Luc Maisonobe <luc@orekit.org>
2023-01-13 13:35:11 +0100

    Fixed compilation warning.

--- hipparchus-core/src/main/java/org/hipparchus/analysis/polynomials/SmoothStepFactory.java
+++ hipparchus-core/src/main/java/org/hipparchus/analysis/polynomials/SmoothStepFactory.java
@@ -139,6 +139,9 @@ public class SmoothStepFactory {
      */
     public static class SmoothStepFunction extends PolynomialFunction {
 
+        /** Serializable UID. */
+        private static final long serialVersionUID = 20230113L;
+        
         /**
          * Construct a smoothstep with the given coefficients. The first element of the coefficients array is the
          * constant term.  Higher degree coefficients follow in sequence.  The degree of the resulting polynomial is the
