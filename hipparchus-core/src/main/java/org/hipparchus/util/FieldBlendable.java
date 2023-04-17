4a359fcadca6042a6d346c902432be80f3b3cc9f
Vincent CUCCHIETTI <vincent.cucchietti@csgroup.eu>
2023-04-17 09:51:41 +0200

    Added CS Group copyright in FieldBlendable

--- hipparchus-core/src/main/java/org/hipparchus/util/FieldBlendable.java
+++ hipparchus-core/src/main/java/org/hipparchus/util/FieldBlendable.java
@@ -1,12 +1,12 @@
-/*
- * Licensed to the Hipparchus project under one or more
+/* Copyright 2002-2023 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The Hipparchus project licenses this file to You under the Apache License, Version 2.0
+ * CS licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
