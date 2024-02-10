28ad27e89b2b237e26a49164ca85f9f8844258d1
Romain Serra <Serrof@users.noreply.github.com>
2024-02-10 14:08:24 +0100

    Update SQPOptimizerSTest.java

--- hipparchus-optim/src/test/java/org/hipparchus/optim/nonlinear/vector/constrained/SQPOptimizerSTest.java
+++ hipparchus-optim/src/test/java/org/hipparchus/optim/nonlinear/vector/constrained/SQPOptimizerSTest.java
@@ -20,35 +20,10 @@ import org.hipparchus.linear.MatrixUtils;
 import org.hipparchus.optim.nonlinear.scalar.ObjectiveFunction;
 import org.junit.Test;
 
-public class SQPOptimizerSTest extends AbstractConstrainedOptimizerTest {
+public class SQPOptimizerSTest extends AbstractSQPOptimizerTest {
 
     protected ConstraintOptimizer buildOptimizer() {
         return new SQPOptimizerS();
-    }
-
-    @Test
-    public void test1() {
-        QuadraticFunction q = new QuadraticFunction(new double[][] { { 4.0, -2.0 }, { -2.0, 4.0 } },
-                                                    new double[] { 6.0, 0.0 },
-                                                    0.0);
-
-        // y = 1
-        LinearEqualityConstraint eqc = new LinearEqualityConstraint(new double[][] { { 0.0, 1.0 } },
-                                                                    new double[] { 1.0 });
-
-
-        // x > 0, y > 0, x + y > 2
-        LinearInequalityConstraint ineqc = new LinearInequalityConstraint(new double[][] { { 1.0, 0.0 }, { 0.0, 1.0 }, { 1.0, 1.0 } },
-                                                                          new double[] { 0.0, 0.0, 2.0 });
-
-
-        doTestProblem(new double[] {  1, 1 },       2.5e-5,
-                      new double[] { -6, 0, 0, 8 }, 2.6e-4,
-                      8, 2.0e-4,
-                      new ObjectiveFunction(q),
-                      new double[] { 3.5, 3.5 },
-                      eqc, ineqc);
-
     }
 
     @Test
